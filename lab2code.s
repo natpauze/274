eecfa8ab2113d4216459e67b3c1ed5edc294112d
Daniel Tavernese <45380503+DanielTavernese@users.noreply.github.com>
2019-01-24 19:40:10 -0500

    updated version

--- lab2code.s
+++ lab2code.s
@@ -19,6 +19,19 @@ _start:
     call PrintHexList
     movia r2, '\n'
     call PrintChar
+    movia r2, LIST
+    ldw r5, N(r0)
+    call CountOdd
+    movia r2, LIST
+    ldw r5, N(r0)
+    call DoubleList
+    movia r2, '\n'
+    call PrintChar
+    movia r2, LIST
+    ldw r5, N(r0)
+    call PrintHexList
+_end:
+br _end
 
 PrintChar:
     subi sp, sp, 8
@@ -61,9 +74,7 @@ PrintHexDigit:
     stw r2, 4(sp)
     stw r4, 0(sp)
     movi r4, 9
-    mov r3, r2
 IF:
-    ldw r2, 0(r3)
     bgt r2, r4, ELSE
 THEN:
     addi r2, r2, '0'
@@ -81,27 +92,78 @@ END_IF:
     ret
     
 PrintHexList:
-    subi sp, sp, 12
-    stw ra, 8(sp)
-    stw r6, 4(sp)
-    stw r5, 0(sp)
+    subi sp, sp, 16
+    stw ra, 12(sp)
+    stw r6, 8(sp)
+    stw r5, 4(sp)
+    stw r3, 0(sp)
+    mov r3,r2
 psh_loop:
     beq r5, r0, end_psh_loop
+    ldw r2,0(r3)
     call PrintHexDigit
     subi r5, r5, 1
-    addi r2, r2, 4
-    mov r6, r2
+    addi r3, r3, 4
     movia r2, ','
     call PrintChar
-    mov r2, r6
     br psh_loop
 end_psh_loop:
-    ldw ra, 8(sp)
-    ldw r6, 4(sp)
-    ldw r5, 0(sp)
-    addi sp, sp, 12
+    ldw ra, 12(sp)
+    ldw r6, 8(sp)
+    ldw r5, 4(sp)
+    ldw r3, 0(sp)
+    addi sp, sp, 16
     ret
 	
+CountOdd:
+    subi sp, sp, 20
+    stw ra, 16(sp)
+    stw r6, 12(sp)
+    stw r5, 8(sp)
+    stw r7, 4(sp)
+    stw r3, 0(sp)
+    mov r3,r2
+pshc_loop:
+    beq r5, r0, end_pshc_loop
+    ldw r2, 0(r3)
+    andi r7, r2, 1
+    add r6, r6, r7
+    subi r5, r5, 1
+    addi r3, r3, 4
+    br pshc_loop
+end_pshc_loop:
+    mov r2, r6
+    call PrintHexDigit
+    ldw ra, 16(sp)
+    ldw r6, 12(sp)
+    ldw r5, 8(sp)
+    ldw r7, 4(sp)
+    ldw r3, 0(sp)
+    addi sp, sp, 20
+    ret
+	
+DoubleList:
+    	subi sp, sp, 16
+    	stw ra, 12(sp)
+    	stw r5, 8(sp)
+	stw r3, 4(sp)
+	stw r2, 0(sp)
+	mov r3,r2
+pshca_loop:
+    	beq r5, r0, end_pshca_loop
+	ldw r2, 0(r3)
+	muli r2, r2, 2
+	stw r2, 0(r3)
+	subi r5, r5, 1
+    	addi r3, r3, 4
+	br pshca_loop
+end_pshca_loop:
+    	ldw ra, 12(sp)
+    	ldw r5, 8(sp)
+	ldw r3, 4(sp)
+	ldw r2, 0(sp)
+    	addi sp, sp, 16
+    ret
  
 	.org	0x1000
         
@@ -111,9 +173,4 @@ end_psh_loop:
 # place byte sized data and strings after word sized data
 
     	MSG: .asciz "ELEC274 Lab2\n"
-
 	.end
-    
-    
-	
-	
